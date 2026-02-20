b1db4e91673bd0b367159b2ef68c25501de4b15d
manojphaju34 <manoj@ukpa.co.uk>
2026-02-20 09:20:47 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Thetips4you" }');
+    res.send('{ "response": "Hello From rentalbox" }');
 });
 
-app.get('/will', function (req, res) {
-    res.send('{ "response": "Hello World" }');
+app.get('/wells', function (req, res) {
+    res.send('{ "response": "Hello from Sterling Wells" }');
 });
 app.get('/ready', function (req, res) {
     res.send('{ "response": " Great!, It works!" }');
